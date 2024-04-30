5d68fcbbb564b42d235ac8fcf281421f81d3d2cb
RaevschiCatalin <raevschicatalin@gmail.com>
2024-04-30 16:58:57 +0300

    secured info using .env files for db configuration

--- soulsync/database/firebase.js
+++ soulsync/database/firebase.js
@@ -1,26 +1,30 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue } from "firebase/database";
+import { config } from 'dotenv';
+config({ path: '../.env' });
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyC7OmpX_i4q0rQXJmWaGAGGiLEs6mx3sKs",
-    authDomain: "proiectpc-5b158.firebaseapp.com",
-    databaseURL: "https://proiectpc-5b158-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "proiectpc-5b158",
-    storageBucket: "proiectpc-5b158.appspot.com",
-    messagingSenderId: "931928611908",
-    appId: "1:931928611908:web:872169d4df2f70b8e8b011",
-    measurementId: "G-LSTBND66SG"
-};
 
 // Initialize Firebase
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+
 const app = initializeApp(firebaseConfig);
+
+// Database reference
+const database = getDatabase(app);
 
 // Add new user
 function writeUserData(userID, email, password) {
-    const db = getDatabase();
-    const reference = ref(db, 'users/' + userID);
+    const reference = ref(database, 'users/' + userID);
     set(reference, {
         email: email,
         password: password
@@ -29,8 +33,7 @@ function writeUserData(userID, email, password) {
 
 // Get data from user by ID
 function readUserData(userID) {
-    const db = getDatabase();
-    const reference = ref(db, 'users/' + userID);
+    const reference = ref(database, 'users/' + userID);
     onValue(reference, (snapshot) => {
         const data = snapshot.val();
         console.log(snapshot.val());
@@ -38,5 +41,5 @@ function readUserData(userID) {
 }
 
 // test
-writeUserData(0,"placeholder","placeholder");
+writeUserData(0, "placeholder", "placeholder");
 readUserData(0);
