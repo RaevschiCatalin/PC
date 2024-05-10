4efd4148ed42ece63397656fcfa27a1e279df977
RaevschiCatalin <raevschicatalin@gmail.com>
2024-05-10 20:55:57 +0300

    quick vercel deploy fix

--- soulsync/app/api/auth/[...nextauth]/route.js
+++ soulsync/app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,19 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId:  '',
-            clientSecret: '',
-        })
-        ],
-    async session({session}) {
-        return session;
-    },
-    async signIn({profile}) {
-        return true;
-    }
-});
-
-export default handler;
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+//
+// const handler = NextAuth({
+//     providers: [
+//         GoogleProvider({
+//             clientId:  '',
+//             clientSecret: '',
+//         })
+//         ],
+//     async session({session}) {
+//         return session;
+//     },
+//     async signIn({profile}) {
+//         return true;
+//     }
+// });
+//
+// export default handler;
