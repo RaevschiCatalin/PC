a5a3be4012f861b54ae1a3f88fe725d5403455fa
AdrianIvanciuc <adrianivanciuc222@gmail.com>
2024-05-11 14:46:00 +0300

    Am adaugat intrebarile si am modificat codul pt ele.

--- soulsync/backend/user.js
+++ soulsync/backend/user.js
@@ -3,5 +3,10 @@ export default class User {
         this.id = id;
         this.email = email;
         this.password = password;
+        this.E = 20;
+        this.A = 14;
+        this.C = 14;
+        this.N = 38;
+        this.O = 8;
     }
 }
--- soulsync/database/firebase.js
+++ soulsync/database/firebase.js
@@ -18,12 +18,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Add new user
-function writeUserData(userID, email, password) {
+function writeUserData(userID, email, password, E, A, C, N, O) {
     const db = getDatabase();
     const reference = ref(db, 'users/' + userID);
     set(reference, {
         email: email,
-        password: password
+        password: password,
+        E: E,
+        A: A,
+        C: C,
+        N: N,
+        O: O
     });
 }
 
