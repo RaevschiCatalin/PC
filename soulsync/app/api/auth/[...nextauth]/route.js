5f3c34edf853eee25faba2907291769f370a022f
RaevschiCatalin <raevschicatalin@gmail.com>
2024-05-11 23:39:55 +0300

    vercel deployment fix

--- soulsync/app/api/auth/[...nextauth]/route.js
+++ soulsync/app/api/auth/[...nextauth]/route.js
@@ -1,27 +1,27 @@
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import { config } from 'dotenv';
-// config({ path: '../../../../.env' });
-//
-// const handler = NextAuth({
-//     providers: [
-//         GoogleProvider({
-//             clientId:  process.env.GOOGLE_ID,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//         })
-//         ],
-//     async session({session}) {
-//         return session;
-//     },
-//     async signIn({profile}) {
-//         try{
-//
-//         }catch (error) {
-//             console.log(error);
-//             return false;
-//         }
-//         return true;
-//     }
-// });
-//
-// export { handler as GET, handler as POST }
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { config } from 'dotenv';
+config({ path: '../../../../.env' });
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId:  process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+        ],
+    async session({session}) {
+        return session;
+    },
+    async signIn({profile}) {
+        try{
+
+        }catch (error) {
+            console.log(error);
+            return false;
+        }
+        return true;
+    }
+});
+
+export { handler as GET, handler as POST }
