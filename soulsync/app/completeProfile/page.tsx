f63380e5783bc62c89b13c4eeb0bd69bb0b37001
RaevschiCatalin <raevschicatalin@gmail.com>
2024-05-15 19:43:34 +0300

    muie la barosani

---
+++ soulsync/app/completeProfile/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-6xl font-bold">Muie la barosani</h1>
+
+        </main>
+    );
+}
