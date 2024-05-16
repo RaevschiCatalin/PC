f327e6654945779c1deac07ce74c7fea803c9b7f
GORTHMAN1 <gabriel.feher03@e-uvt.ro>
2024-05-16 13:14:38 +0300

    navbar & layout changes

--- soulsync/app/page.tsx
+++ soulsync/app/page.tsx
@@ -4,11 +4,6 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-red-700">Hello World!</p>
-      <Link href={'/register'}>Register</Link>
-      <Link href={'/login'}>Login</Link>
-      <Link href={'/forgotPassword'}>Forgot Password</Link>
-    </div>
+    <div></div>
   );
 }
