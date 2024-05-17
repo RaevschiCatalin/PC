e5eec387485dabcc095d5f4821e14a3a991dfbef
RaevschiCatalin <raevschicatalin@gmail.com>
2024-05-17 11:04:37 +0300

    am sters un cometariu urat

--- soulsync/app/match/page.tsx
+++ soulsync/app/match/page.tsx
@@ -3,8 +3,8 @@
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-6xl font-bold">Muie la barosani</h1>
-
+            <h1 className="text-6xl font-bold">Take a look at this person</h1>
+            <h2>*here  would be a person card with the name age city description compatilibility score etc</h2>
         </main>
     );
 }
