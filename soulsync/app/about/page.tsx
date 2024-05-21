097bdfa3160af278234da636a1ab2eef6736fa67
RaevschiCatalin <raevschicatalin@gmail.com>
2024-05-21 13:44:29 +0300

    hui galimii

--- soulsync/app/about/page.tsx
+++ soulsync/app/about/page.tsx
@@ -2,7 +2,6 @@ export default function About() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1 className="text-4xl font-bold">About</h1>
-        // description of our soulmate finder app
         <p className="text-lg">This app is designed to help you find your soulmate.</p>
         <p className="text-lg">We use a sophisticated algorithm to match you with the perfect partner.</p>
         <p className="text-lg">Sign up today to find the love of your life!</p>
