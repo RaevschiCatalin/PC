8ecae652c6d55243502cbccefd443d6c1f04a324
AdrianIvanciuc <adrianivanciuc222@gmail.com>
2024-05-22 19:58:27 +0300

    am nevoie de o variabila globala ce tine minte current user, can do more after that

--- soulsync/backend/handleSubmit.ts
+++ soulsync/backend/handleSubmit.ts
@@ -1,8 +1,16 @@
+import {ref, set} from "firebase/database";
+import {database} from "../database/firebase.js";
 //db push logic in care le bagi in tabele
 //plus logic cu getter a datelor de aici din db
 const pushDataToDatabase = async (data: any) => {
 try {
-       console.log(data);
+        const reference = ref(database, 'profiles/' + 'placeholder%gmail%com');
+        set(reference, {
+            name: data.name,
+            dob: data.yearOfBirth,
+            city: data.city,
+            description: data.description
+        })
     } catch (error) {
         console.error(error);
     }
