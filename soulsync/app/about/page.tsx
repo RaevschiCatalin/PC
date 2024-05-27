f9fa5ed6f6f865a8102d3032c2c5cd75b6548287
RaevschiCatalin <raevschicatalin@gmail.com>
2024-05-27 14:21:30 +0300

    img to Image component change for faster server response

--- soulsync/app/about/page.tsx
+++ soulsync/app/about/page.tsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-4xl font-bold">About</h1>
-            <p className="text-xl font-inter">This app is designed to help you find your soulmate. We understand that finding the right person can be challenging, and we're here to simplify that process for you.</p>
+            <p className="text-xl font-inter">This app is designed to help you find your soulmate. We understand that finding the right person can be challenging, and we&apos;re here to simplify that process for you.</p>
             <p className="text-xl font-inter">We use a sophisticated algorithm to match you with the perfect partner. Our algorithm takes into account your preferences, interests, and values to ensure the best possible matches. By analyzing various factors, we aim to connect you with individuals who truly complement your personality and lifestyle.</p>
             <p className="text-xl font-inter">Sign up today to find the love of your life! Joining our community is the first step towards discovering meaningful connections. Our user-friendly interface makes it easy to create a profile and start browsing potential matches right away.</p>
             <p className="text-xl font-inter">Our mission is to create a safe and welcoming environment for everyone seeking love. We prioritize your privacy and security, ensuring that your personal information is protected at all times. Our support team is always available to assist you with any questions or concerns you may have.</p>
